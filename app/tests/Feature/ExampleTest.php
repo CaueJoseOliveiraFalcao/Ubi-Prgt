a3e7dd9ba4c92d0791d5e4b77d4321836e29b8c3
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2024-07-08 09:02:14 -0400

    o menino de vo vai deizar vovo

--- app/tests/Feature/ExampleTest.php
+++ app/tests/Feature/ExampleTest.php
@@ -13,7 +13,7 @@ class ExampleTest extends TestCase
     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
-
+dsaf
         $response->assertStatus(200);
     }
 }
