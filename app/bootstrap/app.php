1aaceda74e1045e72adba556c8892ded16c6710c
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2024-07-09 13:46:00 -0400

    fix

--- app/bootstrap/app.php
+++ app/bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'isAdmin' => \App\Http\Middleware\CheckPermission::class,
+            'isAdmin' => CheckPermission::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
